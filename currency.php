b1dcfa8c44a9224454a5780661d338ee4a3ea028
Denis K <deniskulygin@gmail.com>
2019-02-03 19:15:02 +0400

    Currency Homework

--- currency.php
+++ currency.php
@@ -2,6 +2,11 @@
 
 $input = end($argv);
 
+if(!checkLessMax($input)) {
+	echo "Value " . $input . " is more then 100000" . PHP_EOL;
+	exit(1);
+};
+
 countCurrency($input);
 
 function countCurrency($sum)
@@ -15,12 +20,18 @@ function countCurrency($sum)
             $sum = $sum - $noteCounter[$i] * $currencyNotes[$i];
         }
     }
+
     echo 'Input: ' . "$inputSum" . PHP_EOL . 'Output: '. PHP_EOL;
+
     for ($i = 0; $i < 9; $i++) {
         if ($noteCounter[$i] != 0) {
             echo ($currencyNotes[$i] . " : " .
                 $noteCounter[$i] . PHP_EOL);
         }
     }
+}
+
+function checkLessMax($input) {
+	return $input <= 100000;
 }
 
