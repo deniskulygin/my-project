b8ca2c728c31eb96e3987198c43cc718d7f368e5
Denis K <deniskulygin@gmail.com>
2019-02-04 00:27:59 +0200

    URL Parser v 1.1

--- url_parser.php
+++ url_parser.php
@@ -5,58 +5,54 @@ $url = end($argv);
 
 $result['scheme'] = getScheme($url);
 $result['host'] = getHost($url);
-getPath($url) === null ? : $result['pass'] = getPath($url);
+getPath($url) === null ? : $result['path'] = getPath($url);
 getQuery($url) === null ? : $result['query'] = getQuery($url);
 getFragment($url) === null ? : $result['fragment'] = getFragment($url);
 
 function getScheme($url)
 {
-	if ((($value = parse_url($url, PHP_URL_SCHEME)) === null)){
-		exit("URL is not Valid! Protocol doesn`t mentioned!").PHP_EOL;
-	} else {
+	if (($value = parse_url($url, PHP_URL_SCHEME)) === null) {
+		exit("URL is not Valid! Protocol doesn`t mentioned!".PHP_EOL);
+	} 
+
 		return $value;
-	}
 }
 
 function getHost($url)
 {
-    if ((($value = parse_url($url, PHP_URL_HOST)) === null)){
-        exit("URL has no Host!").PHP_EOL;
-    } else {
+    if (($value = parse_url($url, PHP_URL_HOST)) === null) {
+        exit("URL has no Host!".PHP_EOL);
+    } 
+
         return $value;
-    }
 }
 
 function getPath($url)
 {
-    if ((($value = parse_url($url, PHP_URL_PATH)) === null)){
+    if (($value = parse_url($url, PHP_URL_PATH)) === null) {
         return null;
-    } else {
+    } 
+
         return $value;
-    }
 }
 
 function getQuery($url)
 {
-    if ((($value = parse_url($url, PHP_URL_QUERY)) === null)){
+    if (($value = parse_url($url, PHP_URL_QUERY)) === null) {
         return null;
-    } else {
+    } 
+
         return $value;
-    }
 }
 
 function getFragment($url)
 {
-    if ((($value = parse_url($url, PHP_URL_FRAGMENT)) === null)){
+    if (($value = parse_url($url, PHP_URL_FRAGMENT)) === null) {
         return null;
-    } else {
+    } 
+
         return $value;
-    }
 }
 
 
 print_r($result);
-
-
-?>
-
