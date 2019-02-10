6a2fab3bbb090cbe82d99c998ec1e9e87071ac27
Denis K <deniskulygin@gmail.com>
2019-02-11 01:45:55 +0200

    Generate and print to CLI 'snake' array v 1.0.2

--- CLIsnake.php
+++ CLIsnake.php
@@ -4,9 +4,9 @@
 isset($argv[1]) ?: exit("Enter quantity of numbers of the square's edge.".PHP_EOL);
 
 if (strtolower($argv[2]) === "dot") {
-	Dot($argv[1]);
+	getDotResult($argv[1]);
 } elseif (strtolower($argv[2]) === "right") {
-	Right($argv[1]);
+	getRightResult($argv[1]);
 } elseif (isset($argv[2])) {
     exit('Second argument must be <dot> OR <right>.'.PHP_EOL);
 } 
@@ -15,7 +15,7 @@ $first = 0;
 
 $number;
 
-function Dot($edge) 
+function getDotResult($edge) 
 {
 	for ($i = 0; $i < $edge; $i++) {
     	for ($j = 0; $j < $edge; $j++) {
@@ -27,7 +27,7 @@ function Dot($edge)
 	}
 }
 
-function Right($edge) 
+function getRightResult($edge) 
 {
 	for ($i = 1; $i <= $edge; $i++) {
 		    global $number;
