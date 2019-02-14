60f1ef945d5ad3282da2164ab6964a0643b21b80
Denis K <deniskulygin@gmail.com>
2019-02-14 12:43:30 +0400

    AP Exercise 1 v 1.0.1

--- Exercise1.php
+++ Exercise1.php
@@ -28,9 +28,9 @@ public function setLastName($lastname)
 public function setGender($gender)
 {
 	if($gender === 'male' || $gender === 'male') {
-	 		$this -> gender = $gender;
-		} else {
-	 		throw new RuntimeException ('Gender can be only <male> or <female>');
+        $this -> gender = $gender;
+    } else {
+        throw new RuntimeException ('Gender can be only <male> or <female>');
 	}
 }
 
@@ -38,9 +38,9 @@ public function setGender($gender)
 public function setStatus($status)
 {
 	if($status === 'freshman' || $status=== 'sophomore' || $status === 'junior' || $status === 'senior') {
-			$this -> status = $status;
-		} else {
-			throw new RuntimeException ('Status can be only <freshman> or <sophomore> or <junior> or <senior>');
+        $this -> status = $status;
+    } else {
+        throw new RuntimeException ('Status can be only <freshman> or <sophomore> or <junior> or <senior>');
 	}
 }
 
@@ -53,47 +53,46 @@ public function setGPA($gpa)
 public function studyTime(int $study_time)
 {
 	if(isset($this -> gpa)) {
-			$this -> study_time = $this -> gpa + log($study_time);
-		} else {
-		 	throw new RuntimeException ('GPA is not set, we cannot calculate Study Time');
-		}
+        $this -> study_time = $this -> gpa + log($study_time);
+    } else {
+        throw new RuntimeException ('GPA is not set, we cannot calculate Study Time');
+    }
 }
 
 public function showMyself()
 {
 	if (isset($this -> firstname)) {
-			echo "First Name: {$this -> firstname}" . PHP_EOL;
-		} else {
-			throw new RuntimeExeption ('First Name is not set.');
+        echo "First Name: {$this -> firstname}" . PHP_EOL;
+    } else {
+        throw new RuntimeExeption ('First Name is not set.');
 	}
 	if (isset($this -> lastname)) {
-	 		echo "Last Name: {$this -> lastname}" . PHP_EOL;
-	 	} else {
-	 		new RuntimeExeption ('Last Name is not set.');
+        echo "Last Name: {$this -> lastname}" . PHP_EOL;
+    } else {
+        new RuntimeExeption ('Last Name is not set.');
 	}
 	if (isset($this -> gender)) {
-			echo "Gender: {$this -> gender}" . PHP_EOL;
-		} else {
-			new RuntimeExeption ('Gender is not set.');
-		}
+        echo "Gender: {$this -> gender}" . PHP_EOL;
+    } else {
+        new RuntimeExeption ('Gender is not set.');
+    }
 	if (isset($this -> status)) {
-			echo "Status: {$this -> status}" . PHP_EOL;
-		} else {
-			throw new RuntimeException ('Status is not set.');
-		}
+        echo "Status: {$this -> status}" . PHP_EOL;
+    } else {
+        throw new RuntimeException ('Status is not set.');
+    }
 	if (isset($this -> gpa)) {
-	 		echo "GPA: {$this -> gpa}" . PHP_EOL;
-		} else {
-		  	throw new RuntimeException ('GPA is not set.');
-		}
-	
-	if (isset($this -> study_time)) {
-			echo "Study Time: {$this -> study_time}" . PHP_EOL;
-		} else {
-			throw new RuntimeException ('Study Time is not set.');
-		}
-}
+        echo "GPA: {$this -> gpa}" . PHP_EOL;
+    } else {
+        throw new RuntimeException ('GPA is not set.');
+    }
 
+	if (isset($this -> study_time)) {
+        echo "Study Time: {$this -> study_time}" . PHP_EOL;
+    } else {
+        throw new RuntimeException ('Study Time is not set.');
+    }
+}
 }
 
 $a = new Student;
