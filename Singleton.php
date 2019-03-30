cdb9201e350fca6c7dd5351a5e8267e2563720f6
Denis K <deniskulygin@gmail.com>
2019-03-30 20:39:02 +0200

    PHP OOP: Solid. Home task Singleton

--- Singleton.php
+++ Singleton.php
@@ -2,63 +2,62 @@
 
 final class DatabaseConnection
 {
-    /** @return DatabaseConnection */
-    private static $instances;
+    /** @return \PDO */
+    private static $db;
 
-    protected function __construct() { }
+    private function __construct()
+    {
+    }
 
-    protected function __clone() { }
+    private function __clone()
+    {
+    }
 
-    /** @throws Exception */
+    /**
+     * @throws Exception
+     */
     public function __wakeup()
     {
         throw new \Exception("Cannot unserialize a DatabaseConnection class");
-    }
-
-    /** @return DatabaseConnection */
-    public static function getInstance(): self
-    {
-        if (!isset(self::$instances)) {
-            self::$instances = new self;
-        }
-
-        return self::$instances;
     }
 
     /**
      * @param string $dsn
      * @param string $username
      * @param string $password
-     * @return PDO|string
-     * @throws \Exception
+     * @return \PDO
+     * @throws Exception
      */
-    public function establishConnection(string $dsn, string $username, string $password)
+    public static function getDbConnection(string $dsn, string $username, string $password): \PDO
+    {
+        if (static::$db !== null) {
+            static::$db = static::establishConnection($dsn, $username, $password);
+        }
+
+        return static::$db;
+    }
+
+    /**
+     * @param string $dsn
+     * @param string $username
+     * @param string $password
+     * @return \PDO
+     * @throws \Exception
+    */
+    private static function establishConnection(string $dsn, string $username, string $password)
     {
         try {
             $connection = new \PDO($dsn, $username, $password);
             if($connection->errorCode()) {
-            //Set common attributes
-            $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            return $connection;
+                //Set common attributes
+                $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                return $connection;
             }
 
             throw new \Exception("Can`t connect to Database");
 
         } catch(\Exception $e) {
-            return $e->getMessage();
-       }
+            echo $e->getMessage();
+        }
     }
 }
-
-function testDatabaseConnectionSingleton()
-{
-    $s1 = DatabaseConnection::getInstance();
-    $s2 = DatabaseConnection::getInstance();
-    if ($s1 === $s2) {
-        echo "Singleton works, both variables contain the same instance.";
-    } else {
-        echo "Singleton failed, variables contain different instances.";
-    }
-}
-
-testDatabaseConnectionSingleton();
