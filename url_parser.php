af1ef30b87fe510dc5b1fdffa3b6916b5997b293
Denis <denis@Deniss-MacBook-Pro.local>
2019-05-03 23:57:10 +0300

    URL Parser v 1.0.1

--- url_parser.php
+++ url_parser.php
@@ -11,74 +11,46 @@ getFragment($url) === null ? : $result['fragment'] = getFragment($url);
 
 function getScheme($url)
 {
-<<<<<<< HEAD
-	if ((($value = parse_url($url, PHP_URL_SCHEME)) === null)){
-		exit("URL is not Valid! Protocol doesn`t mentioned!" . PHP_EOL);
-	}
+    if ((($value = parse_url($url, PHP_URL_SCHEME)) === null)){
+        exit("URL is not Valid! Protocol doesn`t mentioned!" . PHP_EOL);
+    }
 
     return $value;
-=======
-	if (($value = parse_url($url, PHP_URL_SCHEME)) === null) {
-		exit("URL is not Valid! Protocol doesn`t mentioned!".PHP_EOL);
-	} 
-
-		return $value;
->>>>>>> a2c67abca82f415cc474ae0d6078c2a747f4996a
 }
 
 function getHost($url)
 {
-<<<<<<< HEAD
     if ((($value = parse_url($url, PHP_URL_HOST)) === null)){
         exit("URL has no Host!" . PHP_EOL);
     }
 
     return $value;
-=======
-    if (($value = parse_url($url, PHP_URL_HOST)) === null) {
-        exit("URL has no Host!".PHP_EOL);
-    } 
-
-        return $value;
->>>>>>> a2c67abca82f415cc474ae0d6078c2a747f4996a
 }
 
 function getPath($url)
 {
-    if (($value = parse_url($url, PHP_URL_PATH)) === null) {
+    if ((($value = parse_url($url, PHP_URL_PATH)) === null)){
         return null;
-<<<<<<< HEAD
     }
 
     return $value;
-=======
-    } 
-
-        return $value;
->>>>>>> a2c67abca82f415cc474ae0d6078c2a747f4996a
 }
 
 function getQuery($url)
 {
-    if (($value = parse_url($url, PHP_URL_QUERY)) === null) {
+    if ((($value = parse_url($url, PHP_URL_QUERY)) === null)){
         return null;
-<<<<<<< HEAD
     }
     return $value;
-=======
-    } 
-
-        return $value;
->>>>>>> a2c67abca82f415cc474ae0d6078c2a747f4996a
 }
 
 function getFragment($url)
 {
-    if (($value = parse_url($url, PHP_URL_FRAGMENT)) === null) {
+    if ((($value = parse_url($url, PHP_URL_FRAGMENT)) === null)){
         return null;
-    } 
-
+    } else {
         return $value;
+    }
 }
 
 print_r($result);
